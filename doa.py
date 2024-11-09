fd24288016b7780d945a14107a1e3888bb3bd540
Anonumous <iam@not.email>
2024-11-09 23:00:45 +0300

    It was me, Dio.

--- doa.py
+++ doa.py
@@ -1,16 +1,29 @@
 from tuning import Tuning
 import usb.core
 import usb.util
-import time
+#import time
 
-dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
+def find_respeaker():
+    return usb.core.find(idVendor=0x2886, idProduct=0x0018)
 
-if dev:
-    Mic_tuning = Tuning(dev)
-    print(Mic_tuning.direction)
-    while True:
-        try:
-            print(Mic_tuning.direction)
-            time.sleep(1)
-        except KeyboardInterrupt:
-            break
+def get_doa(device):
+    Mic_tuning = Tuning(device)
+    return Mic_tuning.direction
+
+def calc_direction():
+    ...
+
+
+# if __name__ == "__main__":
+#
+#     dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
+#
+#     if dev:
+#         Mic_tuning = Tuning(dev)
+#         print(Mic_tuning.direction)
+#         while True:
+#             try:
+#                 print(Mic_tuning.direction)
+#                 time.sleep(1)
+#             except KeyboardInterrupt:
+#                 break
--- main.py
+++ main.py
@@ -1,30 +1,45 @@
 import vosk, queue, json
 import sounddevice as sd
+import doa
+
+#keyword = "робот"
+auto_define_dev_id = 1
 
 q = queue.Queue()
-
 devices = sd.query_devices()
-print("Select device id: \n", devices)
-dev_id = 0 # default
 
-try:
-  dev_id = int(input())
-except ValueError:
-  print("Using default value: 0")
+if auto_define_dev_id:
+    for id in str(devices).split("\n"):
+        if "ReSpeaker 4 Mic Array" in id:
+            dev_id = int(id.split()[0])
+            break
+else:
+    print("Select device id: \n", devices)
+    dev_id = 0 # default
+
+    try:
+        dev_id = int(input())
+    except ValueError:
+        print("Using default value: 0")
 
 samplerate = int(sd.query_devices(dev_id, 'input')['default_samplerate'])
 
 try:
-  model = vosk.Model("/path/to/vosk-model-ru")
-  with sd.RawInputStream(samplerate=samplerate, blocksize=8000, device=dev_id, dtype='int16', \
-       channels=1, callback=(lambda i, f, t, s: q.put(bytes(i)))):
-    rec = vosk.KaldiRecognizer(model, samplerate)
+    model = vosk.Model(lang="ru")
+    device = doa.find_respeaker()
+    with sd.RawInputStream(samplerate=samplerate, blocksize=8000, device=dev_id, dtype='int16', \
+                            channels=1, callback=(lambda i, f, t, s: q.put(bytes(i)))):
+        rec = vosk.KaldiRecognizer(model, samplerate)
 
-    while True:
-      data = q.get()
-      if rec.AcceptWaveform(data):
-        data = json.loads(rec.Result())["text"]
-        print("Recognized: " + data)
+        while True:
+            data = q.get()
+            if rec.AcceptWaveform(data):
+                voice = json.loads(rec.Result())["text"]
+                #if rec.AcceptWaveform(data):
+                if len(voice) != 0:
+                #if keyword in voice:
+                    result = doa.get_doa(device)
+                    print(f"Recognized \"{voice}\" from {result}")
                 
 except KeyboardInterrupt:
     print('\nDone')
