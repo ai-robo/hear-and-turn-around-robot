077c1fa71cd557403c171737c9fe0f8be27129b7
Anonumous <iam@not.email>
2024-11-25 22:55:21 +0300

    doa_calc is ready, serial and arduino is next

--- doa.py
+++ doa.py
@@ -1,7 +1,7 @@
 from tuning import Tuning
 import usb.core
 import usb.util
-#import time
+import time
 
 def find_respeaker():
     return usb.core.find(idVendor=0x2886, idProduct=0x0018)
@@ -10,20 +10,23 @@ def get_doa(device):
     Mic_tuning = Tuning(device)
     return Mic_tuning.direction
 
-def calc_direction():
-    ...
+def calc_doa(doa_results: list):
+    return round(sum(doa_results) / len(doa_results))
 
 
-# if __name__ == "__main__":
-#
-#     dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
-#
-#     if dev:
-#         Mic_tuning = Tuning(dev)
+if __name__ == "__main__":
+    dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
+    if dev:
+        Mic_tuning = Tuning(dev)
 #         print(Mic_tuning.direction)
-#         while True:
-#             try:
-#                 print(Mic_tuning.direction)
-#                 time.sleep(1)
-#             except KeyboardInterrupt:
-#                 break
+        while True:
+            try:
+                #print(Mic_tuning.direction)
+                dirs = []
+                for i in range(0, 5):
+                    dirs.append(Mic_tuning.direction)
+                    time.sleep(.5)
+                print(f"Result doa (arithmetic average): {calc_doa(dirs)}; direction recorded: {dirs}")
+
+            except KeyboardInterrupt:
+                break
--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import vosk, queue, json
 import sounddevice as sd
 import time
+import threading
 import doa
+import serial
 
-keyword_vars = ['привет робот', 'привет', 'робот']
+keyword_vars = ['привет робот', 'привет', 'робот', 'тест']
 auto_define_dev_id = 1
+records_count = 4
+delay = 0.4 # Delay between records
+
+def move_servo_to():
+    results = []; i = 0
+    while 1:
+        results.append(doa.get_doa(device))
+        i += 1
+        if len(voice) == 0 or i >= records_count: break
+        time.sleep(delay)
+    dir = doa.calc_doa(results)
+    print(f"Servo will moved to {dir}; directions recorded: {results}")
+    #port.write(bytes(str(dir),  'utf-8'))
 
 q = queue.Queue()
 devices = sd.query_devices()
+#port = serial.Serial(port='COM4',  baudrate=115200, timeout=.1)
 
+## Define device id
 if auto_define_dev_id:
     for id in str(devices).split("\n"):
         if "ReSpeaker 4 Mic Array" in id:
@@ -25,12 +42,15 @@ else:
 
 samplerate = int(sd.query_devices(dev_id, 'input')['default_samplerate'])
 
+## Main code
 try:
-    model = vosk.Model("path/to/vosk-model-ru")
+    #model = vosk.Model("path/to/vosk-model-ru")
+    model = vosk.Model(lang="ru")
     device = doa.find_respeaker()
     with sd.RawInputStream(samplerate=samplerate, blocksize=8000, device=dev_id, dtype='int16', \
                             channels=1, callback=(lambda i, f, t, s: q.put(bytes(i)))):
         rec = vosk.KaldiRecognizer(model, samplerate)
+        print("Ready. I'm listening...")
 
         while True:
             data = q.get()
@@ -38,9 +58,12 @@ try:
                 voice = json.loads(rec.Result())["text"]
                 if len(voice) != 0:
                     if any(keyword in voice for keyword in keyword_vars):
+                        move_servo = threading.Thread(target=move_servo_to)
+                        move_servo.start()
                         time.sleep(1)
-                        result = doa.get_doa(device)
-                        print(f"Recognized \"{voice}\" from {result}")
+
+                        dir = doa.get_doa(device)
+                        print(f"Recognized \"{voice}\" from {dir}")
                 
 except KeyboardInterrupt:
     print('\nDone')
